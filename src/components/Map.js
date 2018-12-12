522dbcf660726e72288d018c63d495d04e25d29f
RishabhAtGit <rishabhkumar.sinha@gmail.com>
2018-12-13 00:46:54 +0530

    Added default error page for google map authentication failure

--- src/components/Map.js
+++ src/components/Map.js
@@ -37,6 +37,16 @@ const MapRenderingComponent = withScriptjs(withGoogleMap((props) =>
 ))
 
 class Map extends Component {
+    //checking for authentication failure before rendering the map
+    componentDidMount(){
+        window.gm_authFailure = this.gm_authFailure;
+    }
+    gm_authFailure = () => {
+        this.props.authFailure(
+            "Authentication Failed :(  see javascript console for technical details"
+        );
+    };
+
     render(){
         return(
             <MapRenderingComponent
